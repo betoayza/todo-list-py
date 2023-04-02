76cd5d13919690d15c01be47b477c06505b47bbc
betoayza <aaayza@gmail.com>
2023-04-02 11:27:20 -0300

    Finished

--- main.py
+++ main.py
@@ -44,33 +44,38 @@ while is_running:
         elif option == 2:
             is_running_3 = True
             while is_running_3:
-                ID = int(input("ID to delete: "))
+                try:
+                    ID = int(input("ID to delete: "))
 
-                found = list(filter(lambda t: t.ID == ID, tasks_list))
+                    found = list(filter(lambda t: t.ID == ID, tasks_list))
 
-                if len(found) == 1:
-                    tasks_list.remove(found[0])
-                    print("\nDeleted successfully!")
-                    is_running_3 = False
-                else:
-                    print("\tTask not dound :(\n\tTry other...\n")
-
+                    if len(found) == 1:
+                        tasks_list.remove(found[0])
+                        print("\nDeleted successfully!")
+                        is_running_3 = False
+                    else:
+                        print("\tTask not found :(\n\tTry other...\n")
+                except:
+                    print("\tThat's not an option, ha! Try again...\n")
         else:  # if option == 3
             is_running_4 = True
             while is_running_4:
-                ID = int(input("ID task: "))
+                try:
+                    ID = int(input("ID task: "))
 
-                found = list(filter(lambda t: t.ID == ID, tasks_list))
+                    found = list(filter(lambda t: t.ID == ID, tasks_list))
 
-                if len(found) == 1:
-                    for task in tasks_list:
-                        if task == found[0]:
-                            tasks_list[tasks_list.index(task)].state = "Completed"
-                            print("\nUpdated successfully!")
-                            is_running_4 = False
-                else:
-                    print("\tTask not dound :(\n\tTry other...\n")
-
+                    if len(found) == 1:
+                        for task in tasks_list:
+                            if task == found[0]:
+                                tasks_list[tasks_list.index(
+                                    task)].state = "Completed"
+                                print("\nUpdated successfully!")
+                                is_running_4 = False
+                    else:
+                        print("\tTask not found :(\n\tTry other...\n")
+                except:
+                    print("\tThat's not an option, ha! Try again...\n")
         # print result
         print("\nThe updated list is:")
         if len(tasks_list) == 0:
